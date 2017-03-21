f5acd1d48f7f06d97c897cb09d1e9479bed6107a
zjq41981 <zjq41981@ly.com>
2017-03-21 13:56:44 +0800

    test

---
+++ src/components/first.vue
@@ -0,0 +1,22 @@
+<template>
+ <div id="first">
+    <p>first vue project</p>
+    <p>i am {{author}}</p>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      author: 'yang'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
